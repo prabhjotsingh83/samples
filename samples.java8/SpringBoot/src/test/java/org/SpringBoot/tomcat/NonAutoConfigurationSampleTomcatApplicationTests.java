5c9099c6b6431161fcfb0545886f63f1aa298960
Prabhjot Singh <prabhjot.singh.83@gmail.com>
2019-02-17 18:55:14 -0500

    removing testcases.

--- samples.java8/SpringBoot/src/test/java/org/SpringBoot/tomcat/NonAutoConfigurationSampleTomcatApplicationTests.java
+++ samples.java8/SpringBoot/src/test/java/org/SpringBoot/tomcat/NonAutoConfigurationSampleTomcatApplicationTests.java
@@ -20,15 +20,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -38,7 +29,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import org.SpringBoot.tomcat.NonAutoConfigurationSampleTomcatApplicationTests.NonAutoConfigurationSampleTomcatApplication;
 import org.SpringBoot.tomcat.service.HelloWorldService;
 import org.SpringBoot.tomcat.web.SampleController;
 
@@ -50,36 +40,13 @@ import static org.junit.Assert.assertEquals;
  * @author Dave Syer
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = NonAutoConfigurationSampleTomcatApplication.class)
 @WebAppConfiguration
-@IntegrationTest("server.port=0")
 @DirtiesContext
 public class NonAutoConfigurationSampleTomcatApplicationTests {
 
 	@Value("${local.server.port}")
 	private int port;
 
-	@Configuration
-	@Import({ EmbeddedServletContainerAutoConfiguration.class,
-			DispatcherServletAutoConfiguration.class,
-			ServerPropertiesAutoConfiguration.class, WebMvcAutoConfiguration.class,
-			HttpMessageConvertersAutoConfiguration.class,
-			PropertyPlaceholderAutoConfiguration.class })
-	@ComponentScan(basePackageClasses = { SampleController.class, HelloWorldService.class })
-	public static class NonAutoConfigurationSampleTomcatApplication {
 
-		public static void main(String[] args) throws Exception {
-			SpringApplication.run(PSWebApplication.class, args);
-		}
-
-	}
-
-	@Test
-	public void testHome() throws Exception {
-		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
-				"http://localhost:" + this.port, String.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		assertEquals("Hello World", entity.getBody());
-	}
 
 }
--- samples.java8/SpringBoot/src/test/java/org/SpringBoot/tomcat/SampleTomcatApplicationTests.java
+++ samples.java8/SpringBoot/src/test/java/org/SpringBoot/tomcat/SampleTomcatApplicationTests.java
@@ -19,9 +19,6 @@ package org.SpringBoot.tomcat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
@@ -36,21 +33,12 @@ import static org.junit.Assert.assertEquals;
  * @author Dave Syer
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = PSWebApplication.class)
 @WebAppConfiguration
-@IntegrationTest("server.port:0")
 @DirtiesContext
 public class SampleTomcatApplicationTests {
 
 	@Value("${local.server.port}")
 	private int port;
 
-	@Test
-	public void testHome() throws Exception {
-		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
-				"http://localhost:" + this.port, String.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		assertEquals("Hello World", entity.getBody());
-	}
 
 }
