f4576c7a6cb303cd8d821d3bd513ab1690d9cae6
Prabhjot Singh <prabhjot.singh.83@gmail.com>
2019-09-04 21:47:25 -0400

    Test Funcational interface concepts.

---
+++ samples.java8/CoreConcepts/src/main/java/com/ps/lambda/functional/FuncTest.java
@@ -0,0 +1,12 @@
+package com.ps.lambda.functional;
+
+@FunctionalInterface
+public interface FuncTest {
+
+	void accept();
+	
+	//Second abstract method cannot be in functional interface
+	//void accept2();
+	
+	
+}
