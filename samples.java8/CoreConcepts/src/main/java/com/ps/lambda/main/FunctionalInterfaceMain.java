097c3eae3d2c7d3a2734fcd6a610cc2bb8399dab
Prabhjot Singh <prabhjot.singh.83@gmail.com>
2019-09-04 21:52:05 -0400

    forEach sample on a list

--- samples.java8/CoreConcepts/src/main/java/com/ps/lambda/main/FunctionalInterfaceMain.java
+++ samples.java8/CoreConcepts/src/main/java/com/ps/lambda/main/FunctionalInterfaceMain.java
@@ -1,10 +1,15 @@
 package com.ps.lambda.main;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FunctionalInterfaceMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		List<String> employees = Arrays.asList("Prabhjot","Gurvinder");
+		
+		employees.forEach(System.out::println);
 	}
 
 }
